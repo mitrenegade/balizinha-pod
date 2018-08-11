7c43dbb19c1d7d92bfb9d5ea2f62708956deba46
bobbyren <bobbyren@gmail.com>
2018-08-11 12:16:38 -0400

    added release and refund buttons; fixed status display; added refund call

--- Example/Balizinha/PaymentSubmitViewController.swift
+++ Example/Balizinha/PaymentSubmitViewController.swift
@@ -18,7 +18,8 @@ class PaymentSubmitViewController: UIViewController {
     @IBOutlet weak var labelInfo: UILabel!
     @IBOutlet weak var buttonPay: UIButton!
     @IBOutlet weak var buttonCapture: UIButton!
-    @IBOutlet weak var buttonCancel: UIButton!
+    @IBOutlet weak var buttonRelease: UIButton!
+    @IBOutlet weak var buttonRefund: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,12 +31,17 @@ class PaymentSubmitViewController: UIViewController {
     fileprivate func refresh() {
         guard let event = event else {
             labelInfo.text = "No event!"
+            buttonCapture.isEnabled = false
+            buttonPay.isEnabled = false
+            buttonRelease.isEnabled = false
+            buttonRefund.isEnabled = false
             return
         }
         var string = "Event: \(event.id)\nAmount: \(event.amount!)"
         if paymentInfo == nil {
             buttonCapture.isEnabled = false
-            buttonCancel.isEnabled = false
+            buttonRelease.isEnabled = false
+            buttonRefund.isEnabled = false
             string = "\(string)\nStatus: No payment"
             if let errorString = errorString {
                 string = "Error: \(errorString)"
@@ -44,18 +50,29 @@ class PaymentSubmitViewController: UIViewController {
                 buttonPay.isEnabled = true
             }
         } else if let chargeId = paymentInfo?["chargeId"], let status = paymentInfo?["status"] as? String, status == "succeeded" {
+            buttonPay.isEnabled = false
+
             string="\(string)\nCharge: \(chargeId)"
-            if let captured = paymentInfo?["captured"] as? Bool, !captured {
-                buttonPay.isEnabled = false
+            let captured = paymentInfo?["captured"] as? Bool ?? false
+            let refunded = paymentInfo?["refunded"] as? Bool ?? false
+            switch (captured, refunded) {
+            case (false, false):
                 buttonCapture.isEnabled = true
-                buttonCancel.isEnabled = true
+                buttonRelease.isEnabled = true
+                buttonRefund.isEnabled = false
                 string = "\(string)\nStatus: Hold"
-            } else {
-                buttonPay.isEnabled = false
+            case (true, false):
                 buttonCapture.isEnabled = false
-                buttonCancel.isEnabled = true
-                string = "\(string)\nStatus: \(status)"
+                buttonRelease.isEnabled = false
+                buttonRefund.isEnabled = true
+                string = "\(string)\nStatus: captured"
+            case (_, true):
+                buttonCapture.isEnabled = false
+                buttonRelease.isEnabled = false
+                buttonRefund.isEnabled = false
+                string = "\(string)\nStatus: refunded"
             }
+            
         }
 
         labelInfo.text = string
@@ -67,8 +84,10 @@ class PaymentSubmitViewController: UIViewController {
             submitPayment()
         } else if button == buttonCapture {
             capturePayment()
-        } else if button == buttonCancel {
-            cancelPayment()
+        } else if button == buttonRelease {
+            releasePayment()
+        } else if button == buttonRefund {
+            refundPayment()
         }
     }
     
@@ -77,7 +96,7 @@ class PaymentSubmitViewController: UIViewController {
         guard let event = event else { return }
         let params = ["userId": player.id, "eventId": event.id]
         FirebaseAPIService().cloudFunction(functionName: "holdPayment", method: "POST", params: params) { (results, error) in
-            print("Results \(String(describing: results)) error \(error)")
+            print("Submit payment Results \(String(describing: results)) error \(error)")
             if let error = error as NSError? {
                 self.errorString = error.userInfo["error"] as? String
                 self.paymentInfo = nil
@@ -96,7 +115,7 @@ class PaymentSubmitViewController: UIViewController {
         guard let event = event, let charge = paymentInfo else { return }
         let params: [String: Any] = ["userId": player.id, "eventId": event.id, "chargeId": charge["chargeId"]!]
         FirebaseAPIService().cloudFunction(functionName: "capturePayment", method: "POST", params: params) { (results, error) in
-            print("Results \(String(describing: results)) error \(error)")
+            print("Capture payment Results \(String(describing: results)) error \(error)")
             if let error = error as NSError? {
                 self.errorString = error.userInfo["error"] as? String
                 self.paymentInfo = nil
@@ -110,7 +129,24 @@ class PaymentSubmitViewController: UIViewController {
         }
     }
     
-    fileprivate func cancelPayment() {
-        
+    fileprivate func releasePayment() {
+        refundPayment()
+    }
+
+    fileprivate func refundPayment() {
+        guard let chargeId = paymentInfo?["chargeId"], let eventId = event?.id else { return }
+        FirebaseAPIService().cloudFunction(functionName: "refundCharge", method: "POST", params: ["chargeId": chargeId, "eventId": eventId]) { (results, error) in
+            print("Refund/release payment: result \(results) error \(error)")
+            if let error = error as NSError? {
+                self.errorString = error.userInfo["error"] as? String
+                self.paymentInfo = nil
+            } else {
+                self.paymentInfo = results as? [String: Any]
+                self.errorString = nil
+            }
+            DispatchQueue.main.async {
+                self.refresh()
+            }
+        }
     }
 }
