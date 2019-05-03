1e4c2766f2ad1d46b64d694196e2642f75df82a9
bobbyren <bobbyren@gmail.com>
2019-05-02 21:33:30 -0400

    ActionListViewController can swipe to delete actions

--- Example/Balizinha/ActionListViewController.swift
+++ Example/Balizinha/ActionListViewController.swift
@@ -55,3 +55,18 @@ extension ActionListViewController {
     }
 }
 
+extension ActionListViewController {
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        guard indexPath.section == 0 else { return }
+        let row = indexPath.row
+        guard row < objects.count else { return }
+        if let action = objects[row] as? Action {
+            ActionService.delete(action: action)
+            tableView.reloadRows(at: [IndexPath(row: row, section: 0)], with: .none)
+        }
+    }
+}
