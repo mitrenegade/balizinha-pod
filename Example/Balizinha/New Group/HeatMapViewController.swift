cb0de02612c0c3c97ca0b5fac3109354b93221c6
bobbyren <bobbyren@gmail.com>
2019-05-09 23:31:55 -0400

    added HeatMapViewController

---
+++ Example/Balizinha/New Group/HeatMapViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HeatMapViewController.swift
+//  Balizinha_Example
+//
+//  Created by Bobby Ren on 5/9/19.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+import UIKit
+
+class HeatMapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
