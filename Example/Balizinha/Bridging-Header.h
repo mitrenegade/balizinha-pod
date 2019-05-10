165a92582ac738f79d32afcd78f309cc7e35a279
bobbyren <bobbyren@gmail.com>
2019-05-10 00:18:47 -0400

    changed group name

--- Example/Balizinha/Bridging-Header.h
+++ Example/Balizinha/Bridging-Header.h
@@ -9,6 +9,8 @@
 #ifndef Bridging_Header_h
 #define Bridging_Header_h
 
+// To import GoogleMapUtils using the source code rather than a cocoapod (because they are static libraries):
+// https://github.com/googlemaps/google-maps-ios-utils/issues/17
 #import "GMUMarkerClustering.h"
 #import "GMUHeatMapTileLayer.h"
 #endif /* Bridging_Header_h */
