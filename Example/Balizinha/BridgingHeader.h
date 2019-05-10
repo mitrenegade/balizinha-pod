68ce7b6c478acd8c13004972ff6a10e7973cbdee
bobbyren <bobbyren@gmail.com>
2019-05-09 23:47:33 -0400

    added GMSMap

---
+++ Example/Balizinha/BridgingHeader.h
@@ -0,0 +1,14 @@
+//
+//  BridgingHeader.h
+//  Balizinha
+//
+//  Created by Bobby Ren on 5/9/19.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+#ifndef BridgingHeader_h
+#define BridgingHeader_h
+
+#import <Google-Maps-iOS-Utils/GMUMarkerClustering.h>
+
+#endif /* BridgingHeader_h */
--- Example/Balizinha/New Group/HeatMapViewController.swift
+++ Example/Balizinha/New Group/HeatMapViewController.swift
@@ -7,12 +7,45 @@
 //
 
 import UIKit
+import GoogleMaps
+import GooglePlaces
+import GoogleUtilities
 
 class HeatMapViewController: UIViewController {
 
+    @IBOutlet weak var mapView: GMSMapView!
+    private var heatMapLayer: GMUHeatmapTileLayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        heatmapLayer = GMUHeatmapTileLayer()
+        heatmapLayer.map = mapView
+    }
+    
+    func addHeatmap()  {
+        var list = [GMUWeightedLatLng]()
+        do {
+            // Get the data: latitude/longitude positions of police stations.
+            if let path = Bundle.main.url(forResource: "police_stations", withExtension: "json") {
+                let data = try Data(contentsOf: path)
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                if let object = json as? [[String: Any]] {
+                    for item in object {
+                        let lat = item["lat"]
+                        let lng = item["lng"]
+                        let coords = GMUWeightedLatLng(coordinate: CLLocationCoordinate2DMake(lat as! CLLocationDegrees, lng as! CLLocationDegrees), intensity: 1.0)
+                        list.append(coords)
+                    }
+                } else {
+                    print("Could not read the JSON.")
+                }
+            }
+        } catch {
+            print(error.localizedDescription)
+        }
+        // Add the latlngs to the heatmap layer.
+        heatmapLayer.weightedData = list
     }
 }
