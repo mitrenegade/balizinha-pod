c62551078bd05dde5dd662abe9322144fafe74fb
bobbyren <bobbyren@gmail.com>
2019-05-10 00:44:01 -0400

    added CLLocationManager and permissions

--- Example/Balizinha/Maps/HeatMapViewController.swift
+++ Example/Balizinha/Maps/HeatMapViewController.swift
@@ -18,12 +18,23 @@ class HeatMapViewController: UIViewController {
     private var heatmapLayer: GMUHeatmapTileLayer!
     var locations = [CLLocationCoordinate2D]()
     
+    var locationManager = CLLocationManager()
+    var currentLocation: CLLocation?
+    var zoomLevel: Float = 10.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         heatmapLayer = GMUHeatmapTileLayer()
         heatmapLayer.map = mapView
+        
+        locationManager = CLLocationManager()
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        locationManager.distanceFilter = 50
+        locationManager.startUpdatingLocation()
+        locationManager.delegate = self
         
         load()
     }
@@ -53,5 +64,48 @@ class HeatMapViewController: UIViewController {
         }
         // Add the latlngs to the heatmap layer.
         heatmapLayer.weightedData = list
+    }
+}
+
+extension HeatMapViewController: CLLocationManagerDelegate {
+    
+    // Handle incoming location events.
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let location: CLLocation = locations.last!
+        print("Location: \(location)")
+        
+        let camera = GMSCameraPosition.camera(withLatitude: location.coordinate.latitude,
+                                              longitude: location.coordinate.longitude,
+                                              zoom: zoomLevel)
+        
+        if mapView.isHidden {
+            mapView.isHidden = false
+            mapView.camera = camera
+        } else {
+            mapView.animate(to: camera)
+        }
+    }
+    
+    // Handle authorization for the location manager.
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        switch status {
+        case .restricted:
+            print("Location access was restricted.")
+        case .denied:
+            print("User denied access to location.")
+            // Display the map using the default location.
+            mapView.isHidden = false
+        case .notDetermined:
+            print("Location status not determined.")
+        case .authorizedAlways: fallthrough
+        case .authorizedWhenInUse:
+            print("Location status is OK.")
+        }
+    }
+    
+    // Handle location manager errors.
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        locationManager.stopUpdatingLocation()
+        print("Error: \(error)")
     }
 }
