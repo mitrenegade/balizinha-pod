c5e0b34ad0e910000ec6571ed1821e72c6415242
bobbyren <bobbyren@gmail.com>
2019-05-19 13:45:07 -0400

    added MockVenueService

---
+++ Balizinha/Classes/Mocks/MockVenueService.swift
@@ -0,0 +1,35 @@
+//
+//  MockVenueService.swift
+//  Balizinha_Example
+//
+//  Created by Bobby Ren on 5/18/19.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+import RenderCloud
+
+// this service retains memory
+public class MockVenueService: VenueService {
+    var cities: [String: Any]
+    init() {
+        cities =
+            ["123":
+                ["createdAt": Date().timeIntervalSince1970 - 3600, "name": "skyville", "state": "Cloud", "lat": 39, "lon": -122]
+            ]
+    }
+    
+    override public func getCities(completion: (([City]) -> Void)?) {
+        var results: [City] = []
+        for (key, val) in cities {
+            if let dict = val as? [String: Any] {
+                let object = Balizinha.City(key: key, dict: dict)
+                results.append(object)
+            }
+        }
+        completion?(results)
+    }
+    
+    override public func createCity(_ name: String, state: String?, lat: Double?, lon: Double?, completion: @escaping (City?, NSError?) -> Void) {
+        completion(nil, nil)
+    }
+}
