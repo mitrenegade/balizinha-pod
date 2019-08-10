7a85ba7104aaca3e58aa1214ea49e1aa9d090bd8
bobbyren <bobbyren@gmail.com>
2019-08-10 12:12:26 -0700

    VenuesListViewController is searchable; added Create button

--- Example/Balizinha/VenuesListViewController.swift
+++ Example/Balizinha/VenuesListViewController.swift
@@ -11,11 +11,16 @@ import Firebase
 import Balizinha
 import RenderCloud
 
-class VenuesListViewController: ListViewController {
+class VenuesListViewController: SearchableListViewController {
     var reference: Reference?
-    
+    var venues: [Venue] = []
+
     override var refName: String {
         return "venues"
+    }
+    
+    override var sections: [Section] {
+        return [("Venues", venues)]
     }
     
     override func viewDidLoad() {
@@ -28,6 +33,12 @@ class VenuesListViewController: ListViewController {
         }
 
         super.viewDidLoad()
+
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Create", style: .plain, target: self, action: #selector(createVenue))
+    }
+    
+    @objc func createVenue() {
+        // TODO
     }
 }
 
@@ -46,5 +57,27 @@ extension VenuesListViewController {
 extension VenuesListViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+}
+
+// search and filtering
+extension VenuesListViewController {
+    @objc override func updateSections(_ newObjects: [FirebaseBaseModel]) {
+        venues = newObjects.compactMap { $0 as? Venue }
+        // TODO: filter by distance
+        venues.sort(by: { (p1, p2) -> Bool in
+            guard let t1 = p1.createdAt else { return false }
+            guard let t2 = p2.createdAt else { return true}
+            return t1 > t2
+        })
+    }
+    
+    override func doFilter(_ currentSearch: String) -> [FirebaseBaseModel] {
+        return objects.filter {(_ object: FirebaseBaseModel) in
+            guard let venue = object as? Venue else { return false }
+            let nameMatch = venue.name?.lowercased().contains(currentSearch) ?? false
+            let idMatch = venue.id.lowercased().contains(currentSearch)
+            return nameMatch || idMatch
+        }
     }
 }
