73b60d8b61267576156eeb8ea164dea540ede365
Bobby Ren <bobbyren@gmail.com>
2020-03-21 12:28:53 -0700

    added videoUrl and validVideoUrl

--- Balizinha/Classes/Models/Event.swift
+++ Balizinha/Classes/Models/Event.swift
@@ -220,6 +220,15 @@ public class Event: FirebaseBaseModel {
             update(key: "recurrenceId", value: newValue)
         }
     }
+    
+    public var videoUrl: String? {
+        get {
+            return self.dict["videoUrl"] as? String
+        }
+        set {
+            update(key: "videoUrl", value: newValue)
+        }
+    }
 }
 
 public extension Event {
@@ -291,6 +300,11 @@ extension Event {
             return "\(lat), \(lon)"
         }
         return nil
+    }
+    
+    public var validVideoUrl: URL? {
+        guard let url = videoUrl else { return nil }
+        return URL(string: url)
     }
 }
 
