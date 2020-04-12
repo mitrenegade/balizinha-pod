a059ddcc7bae84526e380e84739672ecb02a9957
Bobby Ren <bobbyren@gmail.com>
2020-04-11 22:49:31 -0700

    PannaServiceManager initialized with stripeClientId; apiService, connectService, and paymentService all created in PannaServiceManager

--- Balizinha/Classes/Helpers/PannaServiceManager.swift
+++ Balizinha/Classes/Helpers/PannaServiceManager.swift
@@ -8,12 +8,22 @@
 //  Cannot be used to initialize services that are not included in Balizinha Pod, such as Firebase, Google Services
 
 import RenderCloud
+import RenderPay
 
 public struct PannaServiceManager {
     static var baseUrl: String?
     static var baseRef: Reference?
-    public static func configure(baseUrl: String?, baseRef: Reference?) {
-        self.baseUrl = baseUrl
-        self.baseRef = baseRef
+    static var stripeClientId: String!
+    public static func configure(baseUrl: String?, baseRef: Reference?, stripeClientId: String) {
+        PannaServiceManager.baseUrl = baseUrl
+        PannaServiceManager.baseRef = baseRef
+        assert(stripeClientId != nil, "Did you forget to set a stripe clientId")
+        PannaServiceManager.stripeClientId = stripeClientId
     }
+    
+    static var apiService = RenderAPIService(baseUrl: PannaServiceManager.baseUrl,
+                                             baseRef: PannaServiceManager.baseRef)
+    static var stripeConnectService: StripeConnectService = StripeConnectService(clientId:PannaServiceManager.stripeClientId, apiService: PannaServiceManager.apiService)
+    static var stripePaymentService: StripePaymentService = StripePaymentService(apiService: PannaServiceManager.apiService)
+
 }
